ae7c53420b5a8b767d55957b2b740fe6b7d33c8e
Bryan Erazo <bryanerazo29@gmail.com>
2020-11-29 16:11:58 -0500

    recursive search and assign as a thread

---
+++ main.c
@@ -0,0 +1,70 @@
+/**
+ * C program to list all files and sub-directories in a directory.
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+pthread_t IDS[100];
+int INDEX=-1;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/**
+ * Lists all files and sub-directories at given path.
+ */
+void* searchRecursively(void *arg){
+    char path[256];
+    char root_path[256];
+    strcpy(root_path,(char *)arg);
+    struct dirent *dp;
+    DIR *dir = opendir(root_path);
+
+    // Unable to open directory stream
+
+    if (!dir) {
+        return NULL;
+
+    }
+    while ((dp = readdir(dir)) != NULL){
+
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        {
+            pthread_mutex_lock(&mutex);
+
+            // Construct of new path from our base path
+            strcpy(path, root_path);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+
+            pthread_create(&IDS[INDEX], NULL, searchRecursively, path);
+            INDEX++;
+            printf("path=%s\n",path);
+
+            pthread_mutex_unlock(&mutex);
+
+        }
+    }
+    closedir(dir);
+
+    for (size_t i = 0; i < INDEX; i++){
+        pthread_join(IDS[i], NULL);
+    }
+    pthread_exit(NULL);
+}
+
+
+int main(int argc, char *argv[argc+1]){
+
+
+    searchRecursively(argv[1]);
+
+    return 0;
+}
+
+
